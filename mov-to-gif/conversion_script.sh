943a85e2c2d6cd045ec8a7590119ce309d5f4719
Nick Borgers <nickborgers@gmail.com>
2023-12-28 12:31:01 -0500

    finally working!

--- mov-to-gif/conversion_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Credit to this SO answer by alexey-kozhevnikov: https://superuser.com/a/836349
-ffmpeg -i /content/input -vf 'fps=5, scale=if(gte(iw\,ih)\,min(1280\,iw)\,-2):if(lt(iw\,ih)\,min(1280\,ih)\,-2)' /tmp/output.gif
-# Credit to this SO answer by pleasestand: https://superuser.com/a/436109
-convert -layers Optimize /tmp/output.gif /content/output_optimized.gif
-
